44e96fa1a9a393ef5821bf8d7a4569d7b4347195
Tom  Z <71478494+leigelaing@users.noreply.github.com>
2021-02-19 12:24:30 +0800

    Add files via upload

---
+++ diyige.java
@@ -0,0 +1,26 @@
+package com.baqn;
+
+public class diyige {
+	public static void main(String[]args){
+		//整型
+		byte a = 10;//(-128-127)
+		short b = 10;
+		int c = 10;
+		long d  = 105613213132168123L;//buj
+		//浮点型
+		float e = 12.856134348f;
+		double v = 1.251651238;
+			//字符型
+		char h = 'd';
+		System.out.println(e);
+        final double pi = 3;
+		double result = pi*2*2;
+		System.out.println(result);
+		*pi = 3.2699;
+		result = pi*2*2;
+		System.out.println(result);
+		
+	
+	}
+
+}
