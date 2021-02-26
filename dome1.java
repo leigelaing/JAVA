c97c44d05e10723b1c1a064bddf08223058ca1b3
Tom  Z <71478494+leigelaing@users.noreply.github.com>
2021-02-26 08:39:49 +0800

    Add files via upload

---
+++ dome1.java
@@ -0,0 +1,16 @@
+package com.baqn;
+/**
+ *二维数组
+ *数组最内层才是基本数据类型
+ */
+public class dome1 {
+	public  static   void   main(String[]args){
+		//声明二维数组
+		int[][] nums;      //二维数组
+		//创建数组
+		nums = new int[3][2];
+		//int[][][]     //三维数组
+	//	int[][][][]    //四维数组
+	}
+
+}
--- zuoye1.java
+++ zuoye1.java
@@ -1,21 +1,43 @@
-package com.bdqn;
+package com.baqn;
 
+import java.util.Random;
 import java.util.Scanner;
 
-/**
- * 输入5个数，输出5个数中最大数和最小数
- */
 public class zuoye1 {
-	public  static void  main(String[]args){
-		Scanner  input = new Scanner(System.in);
-		System.out.print("请输入:");
-		int a = input.nextInt(); 
-		int b = input.nextInt();
-		int c = input.nextInt();
-		int d = input.nextInt();
-		int e = input.nextInt();
-		int max = 0;
-		int min = 0;
-		
-	} 
+	public  static   void   main(String[] args){
+		int []arr = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+         Scanner input = new Scanner(System.in);
+         while(true){
+        	 System.out.println("1:机选；2：自选");
+             System.out.println("请输入>:");
+             int num = input.nextInt(); 
+             switch(num)
+             {
+             case 1: 
+            	 System.out.println("机选开始！");
+            	 int[] arr1 = new int[7];
+            	 Random ran = new Random();
+            		 int index = 0;
+            			for(int i = 0;i <= 6;i++){
+               			 index = ran.nextInt(arr.length);
+            			 arr1[i] = index;
+               			 System.out.print(arr1[i]+" ");
+            			}
+            			 System.out.println();
+            	        break;
+             case 2:
+            	 System.out.println("自选");
+                 break;
+             
+             default:
+            	 
+                 System.out.println("输入有误！请重新输入");
+                 break;
+             } 
+        	 
+        	 
+         }
+        
+	}
+
 }
